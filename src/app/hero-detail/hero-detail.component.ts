57d06306675b9a99ef3c9b7e9c266a8e9b7da531
tiger <pengcheng.zhao@versa-ai.com>
2019-03-20 20:28:51 +0800

    fix: 保存窗口关闭

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,7 +29,7 @@ export class HeroDetailComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
-
+  
   goBack() {
     this.location.back();
   }
